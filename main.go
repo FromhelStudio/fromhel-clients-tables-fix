cb968eddbfcff2dc8ebfbfed8d2e88b4e0594996
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-30 16:11:35 -0300

    chore: connect to db

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
+
+	uri := os.Getenv("MONGODB_URI")
+
+	if uri == "" {
+		panic(uri)
+	}
+
+	conn, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := conn.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
 }
