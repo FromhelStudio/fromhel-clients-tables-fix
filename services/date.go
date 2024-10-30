aed1ebb9b80257bbfb9e436e0db02f0fbc57c58d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-30 15:58:31 -0300

    chore: parse date method

---
+++ services/date.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"fmt"
+	"time"
+)
+
+func ParseDate(dateStr string) (int64, error) {
+	layouts := []string{
+		"Mon Jan 02 2006 15:04:05 GMT-0700",
+		"2006-01-02T15:04:05.999999999Z07:00",
+		"2006-01-02T15:04:05Z",
+		"2006-01-02 15:04:05",
+	}
+
+	for _, layout := range layouts {
+		t, err := time.Parse(layout, dateStr)
+		if err == nil {
+			return t.Unix(), nil
+		}
+	}
+
+	return time.Time{}.Unix(), fmt.Errorf("invalid date format: %s", dateStr)
+}
