5a02f48f782a16c5f93506f69189dfa866e5435f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-30 16:04:14 -0300

    ci: read env

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
 }
