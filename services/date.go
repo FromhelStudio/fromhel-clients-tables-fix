8d353ec48ebfe5d323cd43543aa05694d2097978
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-30 15:59:36 -0300

    chore: remove (MST) annotation

--- services/date.go
+++ services/date.go
@@ -2,10 +2,13 @@ package services
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 func ParseDate(dateStr string) (int64, error) {
+	dateStr = strings.Split(dateStr, " (")[0]
+
 	layouts := []string{
 		"Mon Jan 02 2006 15:04:05 GMT-0700",
 		"2006-01-02T15:04:05.999999999Z07:00",
