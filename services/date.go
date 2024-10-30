54380254222391f239c084967805484c750bff4c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-30 16:22:44 -0300

    chore: recive a interface

--- services/date.go
+++ services/date.go
@@ -6,22 +6,27 @@ import (
 	"time"
 )
 
-func ParseDate(dateStr string) (int64, error) {
-	dateStr = strings.Split(dateStr, " (")[0]
+func ParseDate(dateStr interface{}) (int64, error) {
+	switch v := dateStr.(type) {
+	case string:
+		v = strings.Split(v, " (")[0]
 
-	layouts := []string{
-		"Mon Jan 02 2006 15:04:05 GMT-0700",
-		"2006-01-02T15:04:05.999999999Z07:00",
-		"2006-01-02T15:04:05Z",
-		"2006-01-02 15:04:05",
-	}
-
-	for _, layout := range layouts {
-		t, err := time.Parse(layout, dateStr)
-		if err == nil {
-			return t.Unix(), nil
+		layouts := []string{
+			"Mon Jan 02 2006 15:04:05 GMT-0700",
+			"2006-01-02T15:04:05.999999999Z07:00",
+			"2006-01-02T15:04:05Z",
+			"2006-01-02 15:04:05",
 		}
-	}
 
-	return time.Time{}.Unix(), fmt.Errorf("invalid date format: %s", dateStr)
+		for _, layout := range layouts {
+			t, err := time.Parse(layout, v)
+			if err == nil {
+				return t.Unix(), nil
+			}
+		}
+
+		return time.Time{}.Unix(), nil
+	default:
+		return time.Now().Unix(), fmt.Errorf("invalid date format: %s", dateStr)
+	}
 }
